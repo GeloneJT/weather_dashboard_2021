84548a38cef0f415bb0381e6dbc55400a65d6d8a
Jelani Thomas <jelani13@icloud.com>
2021-02-21 15:37:50 -0500

    added jquery id selectors

---
+++ Assets/js/script.js
@@ -0,0 +1,15 @@
+var APIkey = "62792f0221bde7c0c082e1e71bab13e3";
+var opnWthrAPI = "https://api.openweathermap.org/data/2.5/weather?";
+
+
+var city_input = document.getElementById("cityInput");
+var city_btn = document.getElementById("findMyCity");
+var city_recent = document.getElementById("recentSearch");
+var city_locat = document.getElementById("cityLocat");
+var city_icon = document.getElementById("curWthrCon");
+var city_temp = document.getElementById("curTemp");
+var city_humd = document.getElementById("curHumd");
+var city_windspd = document.getElementById("curWndSpd");
+var city_ndx = document.getElementById("curUndx");
+
+
