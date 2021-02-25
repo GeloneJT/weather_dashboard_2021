eae38b7de8152190668ee4efa66b08daad6bb9e8
Jelani Thomas <jelani13@icloud.com>
2021-02-25 09:09:25 -0500

    one day fetch working and sent to HTML to show location,temp,humidity, and windspd

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -17,15 +17,35 @@ city_btn.addEventListener('click', function () {
     var cityInput = document.getElementById("cityInput").value
 
     console.log(cityInput)
-    fetch("https://api.openweathermap.org/data/2.5/weather?q="+cityInput+"&appid=62792f0221bde7c0c082e1e71bab13e3")
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+cityInput+"&units=imperial"+"&appid=62792f0221bde7c0c082e1e71bab13e3")
         .then(response => response.json())
         .then(json => {
-            console.log(json) 
+            console.log(json)
 
-            city_locat.innertText = 
+            city_locat.innerText = json.name;
+            city_temp.innerText = `Temperature: ${json.main.temp} F`;
+            city_humd.innerText= `Humidity: ${json.main.humidity} %`;
+            city_windspd.innerText =`Wind Speed ${json.wind.speed} mph`;
+            city_locat.value = "";
+        
+        let lat = json.coord.lat;
+        let lon =json.coord.lon;
+        console.log(lat);
+        console.log(lon);
+
+        fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&appid=62792f0221bde7c0c082e1e71bab13e3")
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+
+
+
 
             
+
+        
         })
+    })
 })
 
 
